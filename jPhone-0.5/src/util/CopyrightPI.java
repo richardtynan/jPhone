8a37cd1c20313d8348ffa5a07c5235584a28daaa
richie <richie@richie-netbook>
2012-09-27 22:58:08 +0100

    x

--- jPhone-0.5/src/util/CopyrightPI.java
+++ jPhone-0.5/src/util/CopyrightPI.java
@@ -1,23 +1,23 @@
 /* (C) 2012 by Richard Tynan
- *
- * All Rights Reserved
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- */
-
+*  (C) 2012 by Privacy International
+*
+* All Rights Reserved
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
 package util;
 
 import java.io.File;
@@ -47,7 +47,7 @@ public class CopyrightPI {
 			+ "*\n" + "*/\n";
 
 	public static void main(String[] args) throws Exception {
-		String dir = "src/";
+		String dir = "/home/richie/Gitrepos/jPhone/jPhone-0.5/src";
 		File file = new File(dir);
 		processDirectory(file);
 	}
@@ -68,6 +68,7 @@ public class CopyrightPI {
 				reader.close();
 				if (!contents.startsWith(copyright)) {
 					contents = copyright + contents;
+					System.out.println(files[i]);
 					FileWriter writer = new FileWriter(files[i]);
 					writer.write(contents);
 					writer.close();
